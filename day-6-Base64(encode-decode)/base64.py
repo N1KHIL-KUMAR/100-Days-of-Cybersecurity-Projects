bf07b01a33810d80bd55bda178693a970c20d26c
NIKHIL KUMAR <123356597+N1KHIL-KUMAR@users.noreply.github.com>
2025-06-18 02:30:53 +0530

    Update base64.py

--- day-6-Base64(encode-decode)/base64.py
+++ day-6-Base64(encode-decode)/base64.py
@@ -1,1 +1,29 @@
+import base64
+
+def base64_fun(choice):
+    try:
+        if choice == 1:
+            encode_user = input("Enter your value to encode: ")
+            base64_bytes = base64.b64encode(encode_user.encode("ascii"))
+            return f"Encoded Base64: {base64_bytes.decode('ascii')}"
+        
+        elif choice == 2:
+            decode_user = input("Enter your Base64 value to decode: ")
+            decoded_bytes = base64.b64decode(decode_user)
+            return f"Decoded Text: {decoded_bytes.decode('ascii')}"
+        
+        else:
+            return "[+] Invalid Choice"
+
+    except Exception as e:
+        return f"[+] Error: {e}"
+
+if __name__ == "__main__":
+    print("1. Encode")
+    print("2. Decode")
+    try:
+        choice = int(input("Enter Your Choice: "))
+        print(base64_fun(choice))
+    except ValueError:
+        print("[+] Please enter a valid number.")
 
