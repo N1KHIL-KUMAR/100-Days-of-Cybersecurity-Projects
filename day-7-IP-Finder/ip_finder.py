7cdcf81b230dbc21bfc3ba371611d1314cc58c29
NIKHIL KUMAR <123356597+N1KHIL-KUMAR@users.noreply.github.com>
2025-06-19 13:33:11 +0530

    Update ip_finder.py

--- day-7-IP-Finder/ip_finder.py
+++ day-7-IP-Finder/ip_finder.py
@@ -1,1 +1,26 @@
+#!/usr/bin/python3
+
+import argparse
+import socket
+
+def convert_ip(url):
+    try:
+        ip = socket.gethostbyname(url)
+        return ip
+    except socket.gaierror:
+        return "Invalid domain or connection issue."
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="IP extractor from domain",
+        usage="Example: %(prog)s -d google.com"
+    )
+    parser.add_argument("-d", "--domain", help="Only enter domain name", required=True)
+    args = parser.parse_args()
+
+    ip_address = convert_ip(args.domain)
+    print(f"[+] IP Address of {args.domain} is: {ip_address}")
+
+if __name__ == "__main__":
+    main()
 
