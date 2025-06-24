cc95427211b17a881ae2f90f5358e8714b22fbc7
NIKHIL KUMAR <123356597+N1KHIL-KUMAR@users.noreply.github.com>
2025-06-25 00:04:31 +0530

    Update Caesar_Cipher_Tool.py

--- day-12-Caesar-Cipher-Tool/Caesar_Cipher_Tool.py
+++ day-12-Caesar-Cipher-Tool/Caesar_Cipher_Tool.py
@@ -1,1 +1,23 @@
+#!/usr/bin/python3
+
+import  random
+import string
+
+char = " "+string.ascii_letters + string.digits + string.punctuation
+
+char = list(char)
+
+key = char.copy()
+
+random.shuffle(key)
+
+ecrypted_massage = input("Enter a massage to encryption: ")
+
+cipher_text = ""
+
+for letter in ecrypted_massage:
+    index = key.index(letter)
+    cipher_text += char[index]
+print(f"original massage : {ecrypted_massage}")
+print(f"encrypted massage {cipher_text}")
 
