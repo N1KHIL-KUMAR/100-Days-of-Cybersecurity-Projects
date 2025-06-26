fd7a2611b114f8c8fb288ed037a3d1dc98d19acd
NIKHIL KUMAR <123356597+N1KHIL-KUMAR@users.noreply.github.com>
2025-06-27 00:38:43 +0530

    Update admin_finder.py

--- day-14-admin-finder/admin_finder.py
+++ day-14-admin-finder/admin_finder.py
@@ -1,1 +1,56 @@
+#!/usr/bin/python3
+
+
+import requests
+
+
+admin_paths_dic = [
+    "/admin",
+    "/admin/login",
+    "/admin.php",
+    "/admin.html",
+    "/administrator",
+    "/administrator/login",
+    "/adminpanel",
+    "/controlpanel",
+    "/cpanel",
+    "/backend",
+    "/backend/login",
+    "/dashboard",
+    "/admin-area",
+    "/admin_area",
+    "/adm",
+    "/wp-admin",         # WordPress
+    "/wp-login.php",     # WordPress
+    "/user/login",
+    "/login",
+    "/system",
+    "/console",
+    "/secure",
+    "/manage",
+    "/moderator",
+    "/mod",
+    "/siteadmin",
+    "/admincp",
+    "/memberadmin"
+]
+
+def admin_finder(url):
+    if not url.startswith("http://") and not url.startswith("https://"):
+        url = "https://" + url
+    for paths in admin_paths_dic:
+        admin_url = f"{url}{paths}"
+        try:
+            r = requests.get(admin_url,timeout=2)
+        except requests.RequestException as er:
+            print(f"[+] ERROR >> {er}")
+            exit()
+        try:
+            if r.status_code < 400:
+                print(f"Status code >> [{r.status_code}] -- url >> {admin_url}")
+        except requests.RequestException  as re:
+            print(f"Error {re}")
+
+if __name__ == "__main__":
+    admin_finder(url='example.com')
 
