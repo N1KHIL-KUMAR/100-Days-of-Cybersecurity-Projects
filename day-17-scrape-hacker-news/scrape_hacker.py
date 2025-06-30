5cc1afd855d809a666ed9aa754d6b22498be56a6
NIKHIL KUMAR <123356597+N1KHIL-KUMAR@users.noreply.github.com>
2025-06-30 14:22:31 +0530

    Update scrape_hacker.py

--- day-17-scrape-hacker-news/scrape_hacker.py
+++ day-17-scrape-hacker-news/scrape_hacker.py
@@ -1,1 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
 
+def scrape_hacker_news():
+    url = "https://news.ycombinator.com/"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print("Failed...")
+        return
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    headlines = soup.select('.titleline')
+
+    data = []
+
+    for idx, item in enumerate(headlines, start=1):
+        title = item.get_text()
+        link = item.find('a')['href']
+        print(f"{idx}. {title} ({link})")
+        data.append([idx, title, link])
+
+    with open("hacker_news_headlines.csv", "w", newline="", encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(["No", "Title", "Link"])
+        writer.writerows(data)
+
+    print("\nSaved to hacker_news_headlines.csv")
+
+if __name__ == "__main__":
+    scrape_hacker_news()
