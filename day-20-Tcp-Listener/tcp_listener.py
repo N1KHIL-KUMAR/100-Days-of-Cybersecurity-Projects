7eccc471df0c3776fd3f0929bc49154b411ef33e
NIKHIL KUMAR <123356597+N1KHIL-KUMAR@users.noreply.github.com>
2025-07-04 13:57:43 +0530

    Update tcp_listener.py

--- day-20-Tcp-Listener/tcp_listener.py
+++ day-20-Tcp-Listener/tcp_listener.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python3
 
+# tcp listener 
+
+import socket
+
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+s.bind(("0.0.0.0",4444))
+s.listen(1)
+print("Listening On Ip (0.0.0.0) .....")
+
+conn,addr = s.accept()
+print(f"connection recv form {addr}")
+
+conn.close()
+s.close()
