73192c1527fc5b663989925404e60eeb641b327f
NIKHIL KUMAR <123356597+N1KHIL-KUMAR@users.noreply.github.com>
2025-07-07 07:53:01 +0530

    Update ARP_scanner.py

--- day-22-ARP-Scanner/ARP_scanner.py
+++ day-22-ARP-Scanner/ARP_scanner.py
@@ -1,1 +1,20 @@
+#!/usr/bin/python3
 
+# Arp Scanner
+
+from scapy.all import ARP, Ether, srp
+
+# Target IP range
+target_ip = "172.168.70.0/24"
+
+
+arp_request = ARP(pdst=target_ip)
+ether = Ether(dst="ff:ff:ff:ff:ff:ff") 
+
+packet = ether / arp_request
+
+result = srp(packet, timeout=2, verbose=0)[0]
+
+
+for sent, received in result:
+    print(f"IP: {received.psrc}, MAC: {received.hwsrc}")
