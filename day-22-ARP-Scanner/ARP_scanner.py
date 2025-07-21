9a0680233a493f07fb56a00ec434293a007f0fed
NIKHIL KUMAR <123356597+N1KHIL-KUMAR@users.noreply.github.com>
2025-07-21 21:34:00 +0530

    Update ARP_scanner.py

--- day-22-ARP-Scanner/ARP_scanner.py
+++ day-22-ARP-Scanner/ARP_scanner.py
@@ -9,9 +9,9 @@ target_ip = "172.168.70.0/24"
 
 
 arp_request = ARP(pdst=target_ip)
-ether = Ether(dst="ff:ff:ff:ff:ff:ff") 
+ether = Ether(dst="ff:ff:ff:ff:ff:ff") # mac 
 
-packet = ether / arp_request
+packet = ether / arp_request #checking 
 
 result = srp(packet, timeout=2, verbose=0)[0]
 
