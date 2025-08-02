6b3535422331e156e9adade9ba1e7c525ba9d3c8
NIKHIL KUMAR <123356597+N1KHIL-KUMAR@users.noreply.github.com>
2025-08-02 23:42:29 +0530

    Update files_dow.py

--- day-27-Files-Downloader/files_dow.py
+++ day-27-Files-Downloader/files_dow.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-import requests
+#module
+import requests 
 
 url = 'https://static.getimg.ai/media/getimg_ai_img-josVHBEoFF1WxfOfNCArd.jpeg'
 r = requests.get(url, allow_redirects=True)
-open('ai.png', 'wb').write(r.content)
+open('ai.png', 'wb').write(r.content) 
